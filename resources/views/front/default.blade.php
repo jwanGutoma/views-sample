c63ed0fe1bf956ab4c5e7850253d3102caab181a
John Emerson Buenviaje <buenviaje.je@gmail.com>
2015-07-28 14:33:55 +0800

    updated

--- resources/views/front/default.blade.php
+++ resources/views/front/default.blade.php
@@ -1,9 +1,5 @@
 @extends('layout.base')
 
-@section('header')
-	@include('front.partials.header')
-@endsection
-
 @section('body')
 	<!-- diri ibutang ang header codes. -->
 	@include('front.partials.nav')
